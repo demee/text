78208921051955704be56254b5ce513e14b5010c
Slawomir Demichowicz <slawomir.demichowicz@gmail.com>
2014-02-16 14:49:44 +0000

    Removed spec directory

--- spec/ui/models/message-spec.js
+++
@@ -1,9 +0,0 @@
-require('../../../assets/js/models/message.js');
-
-describe('Testing message.js', function(){
-  it('should contruct message object', function(){
-    var message = new Message();
-
-    expect(message).toBeDefined();
-  });
-});
